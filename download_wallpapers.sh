42372d7b22792eb352732feb501ce202b4dff6b4
Clement Mondary <clement.mondary@gmail.com>
2024-03-05 15:57:00 +0100

    Ajoute download à chaque fin d'URL

--- download_wallpapers.sh
+++ download_wallpapers.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-
-# Définition de la base de l'URL à scraper
+# Define the URL to scrape
 base_url="https://www.wallpaperflare.com/search?wallpaper=futuristic+city&page="
-
-# Création du répertoire wallpapers s'il n'existe pas
+# Create the wallpapers directory if it doesn't exist
 mkdir -p Wallpapers
 
-# Fonction pour extraire et enregistrer les URLs des images d'une page
+# Extract links and save them to the CSV file
 extract_image_urls() {
-    local page_number=$0
+    local page_number=$1
     local page_url="${base_url}${page_number}"
     local image_urls=($(curl -s "$page_url" | grep -o 'itemprop="url" href="[^"]*"' | sed -e 's/itemprop="url" href="//g' -e 's/"//g'))
-
-    # Parcours de chaque URL d'image et enregistrement dans le fichier CSV
+    
     for image_url in "${image_urls[@]}"; do
-        echo "$image_url" >> Wallpapers/image_urls.csv
+        echo "${image_url}/download" >> Wallpapers/image_urls.csv
     done
 
     echo "Extraction des URLs d'images de la page $page_number terminée."
@@ -23,8 +20,6 @@ extract_image_urls() {
 
 # Extraction des URLs des images de la première page
 extract_image_urls 1
-
-# Nombre de pages à traiter (modifier selon le besoin)
 total_pages=10
 
 # Extraction des URLs des images des pages suivantes
